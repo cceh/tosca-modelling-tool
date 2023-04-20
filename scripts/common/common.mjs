1a7e01cd6f63e1df7096598f1787094506f15966
Marcel Schaeben <m.schaeben@uni-koeln.de>
2023-04-20 16:49:29 +0200

    refactor runCommand in build dep scripts: return promise with proper error handling
    This change was necessary to ensure that the `runCommand` function properly handles errors and propagates them up the call stack. Previously, the function would log the error message but still resolve the promise, making it difficult to determine if a command had failed when used in conjunction with other scripts or CI/CD pipelines. By rejecting the promise in case of an error, we can now correctly identify and handle command failures.

--- scripts/common/common.mjs
+++ scripts/common/common.mjs
@@ -3,14 +3,22 @@ import readline from "readline";
 import chalk from "chalk";
 
 export function runCommand(command, params) {
-    const process = execa(command, params)
+    return new Promise((resolve, reject) => {
+        const process = execa(command, params);
 
-    const rl = readline.createInterface({input: process.stdout})
-    const rle = readline.createInterface({input: process.stderr})
+        const rl = readline.createInterface({input: process.stdout});
+        const rle = readline.createInterface({input: process.stderr});
 
-    rl.on(`line`, line => console.log(`${`[${chalk.bold(runCommand.prefix)}: ${command}]`} ${line}`))
-    rle.on(`line`, line => console.log(`${chalk.red(`[${chalk.bold(runCommand.prefix)}: ${command}]`)} ${line}`))
+        rl.on("line", line => console.log(`${`[${chalk.bold(runCommand.prefix)}: ${command}]`} ${line}`));
+        rle.on("line", line => console.log(`${chalk.red(`[${chalk.bold(runCommand.prefix)}: ${command}]`)} ${line}`));
 
-    console.log(`${chalk.bold.green(">")} ${process.spawnargs.join(' ')}`)
-    return process.catch(e => console.log(e.shortMessage))
+        console.log(`${chalk.bold.green(">")} ${process.spawnargs.join(' ')}`);
+
+        process
+            .then(resolve)
+            .catch((error) => {
+                console.log(error.shortMessage);
+                reject(error);
+            });
+    });
 }
